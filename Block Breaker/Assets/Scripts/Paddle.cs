7de55326e181c260f0113e22306a5477c4047fdb
tamioTech <62820237+tamioTech@users.noreply.github.com>
2020-12-24 03:03:32 -0800

    add bricks to level 1
    add gravity
    add collisions
    add walls

---
+++ Block Breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+
+    //Configuration Paramaters
+    [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float paddlePosMin = 1f;
+    [SerializeField] float paddlePosMax = 15f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        Vector2 paddlePos = new Vector2(mousePosInUnits, transform.position.y);
+        paddlePos.x = Mathf.Clamp(paddlePos.x, paddlePosMin, paddlePosMax);
+        transform.position = paddlePos;
+    }
+
+   
+
+}
